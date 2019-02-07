af01e2d9e859942983b84240960d62653e4e8721
ScriptKiddy99 <47433174+ScriptKiddy99@users.noreply.github.com>
2019-02-07 21:48:17 +0000

    Initial Version
    This leverages Regex to parse the inputted data set and translate it into a easy to user Powershell custom object.

---
+++ Parse-ServerInfo.ps1
@@ -0,0 +1,28 @@
+﻿$Content = Get-Content -Path .\ServerInfo.txt -Raw
+
+$RegexPater = [Regex]::new('(?<ServerInfo>(?<SiteCode>SiteCode\s?:\s?.+)(?:\n?\r?\t?\s?)(?<Ip>Ip\s?:\s?.+)(?:\n?\r?\t?\s?)(?<ComputerName>ComputerName\s?:\s?.+)(?:\n?\r?\t?\s?)(?<Manufacturer>Manufacturer\s?:\s?.+)(?:\n?\r?\t?\s?)(?<Model>Model\s?:\s?.+)(?:\n?\r?\t?\s?)(?<OS>OS\s?:\s?.+)(?:\n?)(?<OSArchitecture>OSArchitecture\s?:\s?.+)(?:\n?\r?\t?\s?)(?<OSInstallDate>OSInstallDate\s?:\s?.+)(?:\n?\r?\t?\s?)+(?<TimeZone>TimeZone\s?:\s+.+)(?:\n?\r?\t?\s?)+(?<Servicepack>Servicepack\s?:\s+.+)(?:\n?\r?\t?\s?)+(?<SerialNumber>SerialNumber\s?:\s+.+)(?:\n?\r?\t?\s?)+(?<CPU>CPU\s?:\s+.+)(?:\n?\r?\t?\s?)+(?<TotalRAM>TotalRAM\s?:\s+.+)(?:\n?\r?\t?\s?)+(?<DiskDriveModel>DiskDriveModel\s?:\s?.+)(?:\n?\r?\t?\s?)(?<HDDCapacity>HDDCapacity\s?:\s?.+)(?:\n?\r?\t?\s?)+?(?<HDDSpace>HDDSpace\s?:\s?.+)(?:\n?\r?\t?\s?)+(?<LastUser>LastUser\s?:\s?.+)(?:\n?\r?\t?\s?)+(?<LastReboot>LastReboot\s?:\s?.+)(?:\n?\r?\t?\s?)+(?<BiosData>BiosDate\s?:\s?.+)(?:\n?\r?\t?\s?)+(?<InvetoryDate>InventoryDate\s?:\s?.+))')
+$Results = $RegexPater.Matches($Content)
+
+$ServerObjects = @()
+ForEach($Result in $Results)
+{
+    $ServerObject = [PSCustomObject]@{}
+
+    $Result.Groups | ForEach-Object { if($_.Name -ne "0" -and $_.Name -ne "ServerInfo") {  Add-Member -MemberType NoteProperty -InputObject $ServerObject -Name $_.Name -Value $_.Value.Split(":")[1].Trim() } }
+
+    $ServerObjects += $ServerObject
+}
+
+
+ForEach($Server in $ServerObjects)
+{
+    Write-Host "-------- Server $($Server.ComputerName) --------"
+    Write-Host $Server.SiteCode
+    Write-Host $Server.Ip
+    Write-Host $Server.ComputerName
+    Write-Host $Server.Manufacturer
+    Write-Host $Server.Model
+    Write-Host $Server.OS
+    Write-Host $Server.OSArchitecture
+    # etc.
+}
